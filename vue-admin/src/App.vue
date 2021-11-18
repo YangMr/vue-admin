db90471c8c3dd991ed9c99343202c68c9c4497c5
cstp <759168442@qq.com>
2021-11-18 14:23:08 +0800

    feta:集成element-ui

--- vue-admin/src/App.vue
+++ vue-admin/src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
     <div id="nav">
+      <i class="el-icon-edit"></i>
+      <i class="el-icon-share"></i>
+      <i class="el-icon-delete"></i>
+      <el-button type="primary" icon="el-icon-search">搜索</el-button>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
