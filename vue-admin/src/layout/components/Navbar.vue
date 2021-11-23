e45f600608cab509948f26387f35b043b5a53516
cstp <759168442@qq.com>
2021-11-23 14:21:27 +0800

    feta:渲染用户头像菜单

--- vue-admin/src/layout/components/Navbar.vue
+++ vue-admin/src/layout/components/Navbar.vue
@@ -1,24 +1,65 @@
 <template>
- <div>
- navbar
- </div>
+  <div class="navbar">
+    <div class="right-menu">
+      <el-dropdown trigger="click" class="avatar-container">
+        <div class="avatar-wrapper">
+          <el-avatar
+            shape="square"
+            :size="40"
+            :src="$store.getters.userInfo.avatar"
+          ></el-avatar>
+          <i class="el-icon-s-tools"></i>
+        </div>
+        <el-dropdown-menu slot="dropdown" >
+          <router-link to="/a">
+            <el-dropdown-item>首页</el-dropdown-item>
+          </router-link>
+          <a href="/" target="_blank">
+            <el-dropdown-item>课程主页</el-dropdown-item>
+          </a>
+          <el-dropdown-item divided>退出登录</el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
- name : "", 
- data(){
-  return {
-
-  }
- },
- components : {
-
- },
-}
+  name: "",
+  data() {
+    return {};
+  },
+  components: {},
+};
 </script>
 
+<style scoped lang="scss">
+.navbar{
+    height:50px;
+    background-color:#fff;
+    box-shadow: 0 1px 4px rgba(0, 21, 41, 0.08);
+    overflow: hidden;
+    position: relative;
 
-<style scoped>
+    .right-menu{
+        float: right;
+        display : flex;
+        align-items : center;
+        padding-right:16px;
 
+        .avatar-container{
+            cursor: pointer;
+
+            .avatar-wrapper{
+                margin-top:5px;
+                position: relative;
+                .el-avatar{
+                    background: none;
+                    margin-right:12px;
+                }
+            }
+        }
+    }
+}
 </style>
--- vue-admin/src/store/getters.js
+++ vue-admin/src/store/getters.js
@@ -2,5 +2,6 @@ export const getters = {
     token : state => state.user.token,
     hasUserInfo : state => {
         return state.user.userInfo
-    }
+    },
+    userInfo :  state => state.user.userInfo
 }
